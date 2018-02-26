a8fb214cbc5f0ad0c7db048bb875f77f253d90cd
Ricardo <richgarcia459@gmail.com>
2018-02-26 17:52:50 +0100

    Application base
    Por ahora la aplicación cuenta con los módulos de mostrar producto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// routes
+import { RoutingModule, AppRoutingProviders } from './app.routing';
+import {HttpClientModule} from '@angular/common/http';
 
+// components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ProductListComponent } from './components/producto/product-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AppRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
